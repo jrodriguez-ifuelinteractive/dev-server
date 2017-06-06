ee6910400133329a9b5a5934dfe0baa57a69a5f3
Jesus Rodriguez <jrodriguez@ifuelinteractive.com>
2017-06-06 08:25:37 -0400

    Added checksum signature check.

--- scripts/php/composer/init.sh
+++ scripts/php/composer/init.sh
@@ -2,8 +2,11 @@
 
 PHP_BIN=`which php`
 
+COMPOSER_SIG=`curl https://composer.github.io/installer.sig`
+
 cd /vagrant
 $PHP_BIN -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+$PHP_BIN -r "if (hash_file('SHA384', 'composer-setup.php') === '$COMPOSER_SIG') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 $PHP_BIN composer-setup.php
 $PHP_BIN -r "unlink('composer-setup.php');"
 
