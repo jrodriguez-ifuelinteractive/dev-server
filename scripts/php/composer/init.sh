66ef29608d0a035f134ad973e4aa9e759d27cfaf
Jesus Rodriguez <jrodriguez@ifuelinteractive.com>
2017-06-06 08:10:50 -0400

    Removed composer checksum check.

--- scripts/php/composer/init.sh
+++ scripts/php/composer/init.sh
@@ -4,7 +4,6 @@ PHP_BIN=`which php`
 
 cd /vagrant
 $PHP_BIN -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-$PHP_BIN -r "if (hash_file('SHA384', 'composer-setup.php') === '55d6ead61b29c7bdee5cccfb50076874187bd9f21f65d8991d46ec5cc90518f447387fb9f76ebae1fbbacf329e583e30') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 $PHP_BIN composer-setup.php
 $PHP_BIN -r "unlink('composer-setup.php');"
 
